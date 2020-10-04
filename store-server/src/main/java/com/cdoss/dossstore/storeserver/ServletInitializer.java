ef86af227e56bc4fce6003e1ee6a04dd47fc501c
Chandy Doss <chandy.doss@gmail.com>
2020-10-04 01:14:06 -0400

    created maven project with modules

---
+++ store-server/src/main/java/com/cdoss/dossstore/storeserver/ServletInitializer.java
@@ -0,0 +1,13 @@
+package com.cdoss.dossstore.storeserver;
+
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+public class ServletInitializer extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(StoreServerApplication.class);
+	}
+
+}
