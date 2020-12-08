debe1b5ccbb17c63aeb5d17259a48465f7320e19
Chandy Doss <chandy.doss@gmail.com>
2020-12-08 01:52:45 -0500

    updating react client

--- store-client/src/main/webapp/src/App.js
+++ store-client/src/main/webapp/src/App.js
@@ -1,15 +1,50 @@
-import React from "react";
+import { Component } from "react";
 import "./App.css";
-import HomePageList from "./HomePageList";
+import HomePageList from "./components/homepage/HomePageList";
+import { logo } from "../../doss-store-media/src/main/media/logo";
+
+function Header({ name }) {
+  return (
+    <header>
+      <img src={logo} alt="" />
+      <h1>{name}'s Kitchen</h1>
+    </header>
+  );
+}
+
+const dishes = ["Cheeseburger", "Hamburger", "Chow Foon", "Spagetti"];
+const dishObjects = dishes.map((dish, i) => ({
+  id: i,
+  title: dish,
+}));
+console.log(dishObjects);
+function Main({ adjective, dishes }) {
+  return (
+    <section>
+      <p>We Serve the Most {adjective} food around.</p>
+      <ul style={{ textAlign: "left" }}>
+        {dishes.map((dish) => (
+          <li key={dish.id}>{dish.title}</li>
+        ))}
+      </ul>
+    </section>
+  );
+}
+
+function Footer({ year }) {
+  return (
+    <footer>
+      <p>Copyright {year}</p>
+    </footer>
+  );
+}
 
 function App() {
   return (
     <div className="App">
-      <div>
-        <h1>Theary Store</h1>
-      </div>
-
-      <HomePageList />
+      <Header name="Don" />
+      <Main adjective="Amazing" dishes={dishObjects} />
+      <Footer year={new Date().getFullYear()} />
     </div>
   );
 }
